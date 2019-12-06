f18e4d6df3f7a91f60b91a1ae6cc50c921fccc5b
chetan dev <chetan.d@purplle.com>
2019-12-06 11:35:47 +0530

    health checkup

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var redis = require("redis");
+var Promise = require('bluebird')
 
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
 
-var fooStatus = require('redis-status')({
-  port: process.env.REDIS_PORT,
-  host: process.env.REDIS_HOST
+var client = redis.createClient({
+    //port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+    port: 7000
 });
- 
-router.get('/', function(req, res) {
-  fooStatus.checkStatus(function(err) {
-  	if(err){
-  		res.status(500).send('error')
-  	}else{
-  		res.send('OK');
-  	}
-    
-  });
+
+
+client.on("error", function(err) {
+    console.log("Error " + err);
+});
+
+
+client.on('connect', function() {
+    console.log('redis connected');
+});
+
+
+
+router.get('/',  function(req, res) {
+   console.log(client.connected)
+   res.send(client.connected)
+
 });
 
 module.exports = router;
